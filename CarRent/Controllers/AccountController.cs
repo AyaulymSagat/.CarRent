b38cfaa395c0a4161f01581d4cfe8de1b32970d7
AyaulymSagat <47839407+AyaulymSagat@users.noreply.github.com>
2019-11-30 07:42:12 +0600

    commit

---
+++ CarRent/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRent.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRent.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index","home");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result= await userManager.CreateAsync(user,model.Password);
+
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user,isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model,string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+              
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password,model.RememberMe,false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("index", "home");
+                    }
+                    
+                }
+
+
+                ModelState.AddModelError("", "Invalid Data");
+               
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ CarRent/Data/MyAppDataContext.cs
@@ -0,0 +1,25 @@
+﻿using CarRent.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.Data
+{
+    public class MyAppDataContext : IdentityDbContext
+    {
+        public MyAppDataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Passport> Passports { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<RentedCars> RentedCars { get; set; }
+  
+    }
+}
